80c3a79ad935778944d49c52a233b10d259b6299
CannedShroud <hypersync112@gmail.com>
2020-09-24 22:50:20 +0530

    Build Header

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,42 @@
+import { IconButton } from "@material-ui/core";
+import { ContactMail, Face, Facebook, Room } from "@material-ui/icons";
 import React from "react";
 import "./Header.css";
+import logo from "./logo.png";
 
 function Header() {
   return (
     <div className="header">
-      <h1> I am header</h1>
+      <div className="header__left">
+        <img src={logo} alt="LOGO" className="header__logo" />
+        <div className="header__title">
+          <h1>Goa Dental Studio</h1>
+        </div>
+      </div>
+      <div className="header__right">
+        <div className="header__nav">
+          <div className="header__navitem">
+            <h3>ABOUT US</h3>
+          </div>
+          <div className="header__navitem">
+            <h3>TESTIMONIALS</h3>
+          </div>
+          <div className="header__navitem">
+            <h3>CONTACT US</h3>
+          </div>
+        </div>
+        <div className="header__icons">
+          <IconButton>
+            <Room className="header__icon" />
+          </IconButton>
+          <IconButton>
+            <Facebook className="header__icon" />
+          </IconButton>
+          <IconButton>
+            <ContactMail className="header__icon" />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
