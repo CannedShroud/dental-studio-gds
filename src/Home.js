a7da2208f14d778a8e03b40999544cb24ece3c33
CannedShroud <hypersync112@gmail.com>
2020-09-24 23:57:54 +0530

    Build main Landing part

--- src/Home.js
+++ src/Home.js
@@ -2,15 +2,26 @@ import React from "react";
 import "./Home.css";
 import Header from "./Header";
 import Footer from "./Footer";
+import chair from "./chair2.jpg";
+import brandingLogo from "./cannon-dental.png";
 
 function Home() {
   return (
     <div className="home">
       <Header />
       <div className="home__body">
-        <h1>I am home</h1>
+        <img src={chair} alt="Image" className="home__image" />
+        <div className="home__container">
+          <div className="home__landing">
+            <div className="home__branding">
+              <img src={brandingLogo} alt="" />
+              {/* <h1>Goa Dental Studio</h1> */}
+              <h3>Giving you a smile you're happy to show off</h3>
+            </div>
+          </div>
+        </div>
       </div>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
